c4d22f94a3e0d7f8bcaebbb7922bead38be8fd2a
Anton-Vovk <mr.vovk.anton@gmail.com>
2021-03-25 23:25:43 +0200

    Update

--- index.js
+++ index.js
@@ -1,31 +1,190 @@
-// ----------------------------1-7---------------------
+// ----------------------------1-9---------------------
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Просторная квартира в центре",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Генри",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// const aptRating = apartment["rating"];
+// const aptDescr = apartment["descr"];
+// const aptPrice = apartment["price"];
+// const aptTags = apartment["tags"];
+// console.log(firstTag, lastTag);
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Генри Сибола";
+// apartment.tags.concat("trusted");
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = { country: "Ямайка", city: "Кингстон" };
+
+// ----------------------------9---------------------
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+  [emailInputName]: "henry.carter@aptmail.com",
+  [passwordInputName]: "jqueryismyjam",
+};
+
+// ----------------------------10---------------------
+// const keys = [];
+// const values = [];
+
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// ----------------------------11---------------------
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Просторная квартира в центре";
+// apartment.rating = 4;
+// apartment.price = 2153;
+
+// for (const key in apartment) {
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+// }
+
+// ----------------------------12---------------------
+// function countProps(object) {
+//   let propCount = 0;
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     }
+//   }
+//   return propCount;
+// }
+
+console.log(countProps({ name: "Mango", age: 2 })); //2
+
+// ----------------------------13---------------------
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+
+// const keys = Object.keys(apartment);
+// for (const key of keys){
+//   values.push(apartment[key]);
+// }
+
+// ----------------------------14---------------------
+function countProps(object) {
+  let propCount = 0;
+
+  propCount = Object.keys(object).length;
+
+  return propCount;
+}
+
+// ----------------------------15---------------------
 const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Просторная квартира в центре',
+  descr: "Просторная квартира в центре",
   rating: 4,
   price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Генри',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
 };
-const ownerEmail = apartment.owner.email;
-const numberOfTags = apartment.tags.length;
-const firstTag = apartment.tags[0];
-const lastTag = apartment.tags[apartment.tags.length - 1];
-const aptRating = apartment['rating'];
-const aptDescr = apartment['descr'];
-const aptPrice = apartment['price'];
-const aptTags = apartment['tags'];
-console.log(firstTag, lastTag);
-apartment.price  = 5000;
-apartment.rating = 4.7;
-apartment.owner.name = 'Генри Сибола';
-apartment.tags.concat('trusted');
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = { country :'Ямайка', city :'Кингстон'};
 
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
 
+// ----------------------------16---------------------
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  const values = Object.values(salaries);
+  for (const value of values) {
+    totalSalary += value;
+  }
+
+  return totalSalary;
+}
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 })); //400
+
+// ----------------------------17---------------------
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+const hexColors = [];
+const rgbColors = [];
+
+for (const color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
+
+// ----------------------------18---------------------
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Сканер", price: 2700, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+
+//   return null;
+// }
+// console.log(getProductPrice("Захват"));
+
+// ----------------------------19---------------------
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  const result = [];
+
+  for (const product of products) {
+    if (product[propName]) {
+      result.push(product[propName]);
+    }
+  }
+  return result;
+}
+
+// ----------------------------20---------------------
+function calculateTotalPrice(productName) {
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.quantity * product.price;
+    }
+  }
+  return 0;
+}
+
+// ----------------------------21---------------------
